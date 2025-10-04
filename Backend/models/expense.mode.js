9f762452ffab50d9ffeb7e3185e3b4da9d370e3c
HetSavani36 <hetsavani36@gmail.com>
2025-10-04 10:44:09 +0530

    added expense model

---
+++ Backend/models/expense.mode.js
@@ -0,0 +1,66 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const ExpenseStatus = Object.freeze({
+  DRAFT: "Draft",
+  SUBMITTED: "Submitted",
+  PENDING: "Pending",
+  APPROVED: "Approved",
+  REJECTED: "Rejected",
+});
+
+const ExpenseSchema = new Schema(
+  {
+    employee: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    description: {
+      type: String,
+      required: [true, "Description is required."],
+      trim: true,
+    },
+    date: {
+      type: Date,
+      required: [true, "Date is required."],
+    },
+    category: {
+      type: String,
+      required: [true, "Category is required."],
+      enum: ["Food", "Travel", "Accommodation", "Software", "Other"], // Example enum
+      trim: true,
+    },
+    paidBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    remarks: {
+      type: String,
+      default: "None",
+      trim: true,
+    },
+    amount: {
+      type: Number,
+      required: [true, "Amount is required."],
+      min: [0.01, "Amount must be greater than zero."],
+    },
+    currency: {
+      code: { type: String },
+      name: { type: String },
+      symbol: { type: String },
+    },
+    status: {
+      type: String,
+      required: [true, "Status is required."],
+      enum: Object.values(ExpenseStatus),
+      default: ExpenseStatus.DRAFT,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Expense", ExpenseSchema);
