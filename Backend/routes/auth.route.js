a5451db6786d76f72996fba7d8429ab3d73230ca
HetSavani36 <hetsavani36@gmail.com>
2025-10-04 09:20:59 +0530

    changed auth route

--- Backend/routes/auth.route.js
+++ Backend/routes/auth.route.js
@@ -4,46 +4,23 @@ import {
   logout,
   meController,
   register,
-  login,
-  verifyOtp,
-  resendOtp,
-  authenticateController,
-  callbackController,
-  userRegisterUsingEmail,
-  loginUsingEmail,
-  adminRegisterUsingEmail,
+  login
 } from "../controllers/auth.controller.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 import authorizeRole from "../middlewares/authorizableRole.js";
 
 const router = Router();
 
-//email + otp
-router.post("/register", register);
-router.post("/login", login);
-router.post("/logout",authorizeRole("user", "admin", "owner"), logout);
+router.post("/logout", authorizeRole("employee", "admin", "manager"), logout);
 router.post("/refresh-token", refreshController);
-router.post("/verifyOtp", verifyOtp);
-router.post("/resendOtp", resendOtp);
 router.get(
   "/me",
   verifyJWT,
-  authorizeRole("user", "admin", "owner"),
+  authorizeRole("employee", "admin", "manager"),
   meController
 );
 
-
-//google
-router.get("/google", authenticateController);
-router.get("/google/callback", callbackController);
-router.post("/refresh-token", refreshController);
-router.post("/logout", logout);
-router.get("/me", verifyJWT, meController);
-
-//email
-router.post("/register",userRegisterUsingEmail)
-router.post("/login",loginUsingEmail)
-router.post("/admin/register",adminRegisterUsingEmail)
-
+router.post("/register",register)
+router.post("/login",login)
 
 export default router;
