be4b7811bc863f19f32b0716f1d4c0da6726be14
23DCS040_HARMIT <harmitjetani@gmail.com>
2025-10-04 15:35:42 +0530

    Add backend directory from the main branch.

---
+++ Backend/middlewares/authorizableRole.js
@@ -0,0 +1,11 @@
+const authorizeRole = (...allowedRoles) => {
+  return (req, res, next) => {
+    if (!req.user || !allowedRoles.includes(req.user.role)) {
+      return res.status(403).json({ message: "Forbidden: insufficient role" });
+    }
+    next();
+  };
+};
+
+
+export default authorizeRole
