b750776de19155cfcc542c64e1e830564ab8e5fb
HetSavani36 <hetsavani36@gmail.com>
2025-10-04 14:51:50 +0530

    enhanced admin analytics

--- Backend/routes/dashboard.route.js
+++ Backend/routes/dashboard.route.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
-import {
-    
-} from "../controllers/dashboard.controller.js";
+import { getManagerAnalytics ,getEmployeeAnalytics} from "../controllers/dashboard.controller.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
-
+import authorizeRole from "../middlewares/authorizableRole.js";
 
 const router = Router();
 router.use(verifyJWT)
+
+router.get("/manager",authorizeRole("manager"),getManagerAnalytics)
+router.get("/employee", authorizeRole("employee"), getEmployeeAnalytics);
 
 export default router;
